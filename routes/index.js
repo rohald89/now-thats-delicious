9e5d12bc5e95bff8de1b8e9b8e191c5b13c78d2b
Rohald van Merode <rohald_89@live.nl>
2021-02-13 19:47:57 +0100

    added forgot password feature

--- routes/index.js
+++ routes/index.js
@@ -53,4 +53,11 @@ router.post('/account', catchErrors(userController.updateAccount))
 
 router.get('/logout', authController.logout);
 
+router.post('/account/forgot', catchErrors(authController.forgot));
+router.get('/account/reset/:token', catchErrors(authController.reset));
+router.post('/account/reset/:token', 
+    authController.confirmedPasswords, 
+    catchErrors(authController.update),
+);
+
 module.exports = router;
