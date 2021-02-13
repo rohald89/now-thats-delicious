c92df23de0ccee1a8bfd3b5e0eda06a138a4fb4c
Rohald van Merode <rohald_89@live.nl>
2021-02-13 17:11:08 +0100

    added logout functionality and added gravatar

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,10 @@ const { catchErrors } = require('../handlers/errorHandlers');
 // Do work here
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
-router.get('/add', storeController.addStore);
+router.get('/add', 
+    authController.isLoggedIn,
+    storeController.addStore
+);
 
 router.post('/add', 
     storeController.upload, 
@@ -30,6 +33,7 @@ router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 router.get('/login', userController.loginForm);
+router.post('/login', authController.login);
 router.get('/register', userController.registerForm);
 
 // 1 validate the registration data
@@ -40,5 +44,7 @@ router.post('/register',
     userController.register,
     authController.login
 );
+
+router.get('/logout', authController.logout);
 
 module.exports = router;
